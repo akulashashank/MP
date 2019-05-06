80f4e541f60e903010159a9169ab0ff32f9f56a2
akulashashank <39285340+akulashashank@users.noreply.github.com>
2019-05-06 08:32:19 +0530

    Add files via upload

--- MiniProjectSql.sql
+++ MiniProjectSql.sql
@@ -3,7 +3,7 @@ use Rajnikanth
 create schema Raj_174851
 drop table Raj_174851.Customer
 drop table Raj_174851.InsuranceProducts
-drop table Customer
+drop table Raj_174851.Customer
 select * from Customer
 create table Customer
 (
@@ -19,29 +19,41 @@ create table Customer
 	CreatedDate date default getdate()
 )
 
+alter table customer add Role varchar(15)
 drop table customer
+delete from Policy
+delete from Endorsement
+delete from Documents
+delete from InsuranceProducts
+delete from customer
 insert into Customer(Password,Name,Address,Telephone,Gender,DOB,isSmoker,Hobbies) values('dsd','dsad','dsad','32332','Male','2019-09-09','Smoker','wdw')
 select * from Customer
 select * from InsuranceProducts
+select * from Endorsement
+select * from Documents
+select * from Policy
 
+update Customer set Role='Admin' where CustomerId=101
 create table InsuranceProducts
 (
 	Policyno int not null primary key identity,
 	ProductName varchar(50) not null,
-	PremiumPaymentFrequency varchar(10) not null,
+	PremiumPaymentFrequency varchar(10) not null ,
 	SumAssured money not null,
-	BasePremium money not null,
 	TotalPremium money not null,
 	TotalPayment money not null,
 	UplaodedDate date default getdate()
 )
+
 use Rajnikanth
+alter table InsuranceProducts add check(PremiumPaymentFrequency in ('Monthly', 'Quarterly', 'Halfyearly', 'Annually'))
+alter table InsuranceProducts drop column BasePremium
 select * from InsuranceProducts
 delete InsuranceProducts where Policyno = 2
 drop table InsuranceProducts
 select * from Policy where Policyno=4
 create table Policy
-(	
+(
 	Policyno int not null foreign key references InsuranceProducts(Policyno),
 	CustomerId int not null foreign key references Customer(CustomerId),
 	ProductId int not null primary key Identity,
@@ -53,8 +65,8 @@ create table Policy
 	BoughtDate date default getdate(),
 	UpdatedDate date 
 )
-insert into Policy(Policyno, CustomerId, Nominee, Relation, NomineeAddress, NomineeTelephone, NomineeDOB, BoughtDate) values(4, 100, 'Rajni', 'Freind', 'Chennai', '999999999', '2019-09-09', '2018-02-09')
-insert into Policy(Policyno, CustomerId, Nominee, Relation, NomineeAddress, NomineeTelephone, NomineeDOB, BoughtDate) values(5, 100, 'Rajni', 'Freind', 'Chennai', '999999999', '2019-09-09', '2018-02-09')
+insert into Policy(Policyno, CustomerId, Nominee, Relation, NomineeAddress, NomineeTelephone, NomineeDOB, BoughtDate) values(8, 108, 'Rajni', 'Freind', 'Chennai', '999999999', '2019-09-09', '2018-02-09')
+insert into Policy(Policyno, CustomerId, Nominee, Relation, NomineeAddress, NomineeTelephone, NomineeDOB, BoughtDate) values(5, 108, 'Rajni', 'Freind', 'Chennai', '999999999', '2019-09-09', '2018-02-09')
 
 select * from Policy
 create table Documents
@@ -65,8 +77,10 @@ create table Documents
 	Photo image not null,
 	UploadedDate date default getdate()
 )
+alter table Documents add Photo nvarchar(500)
 ALTER TABLE Documents
 ALTER COLUMN Photo nVarchar(max);
+delete from Endorsement
 create table Endorsement
 (
 	EndorsementId int primary key not null,
@@ -79,6 +93,26 @@ create table Endorsement
 	ApprovedDate date,
 	RejectedDate date
 )
+alter table	Endorsement drop column ChangesMade
+alter table	Endorsement add ChangedPremiumPaymentFrequency varchar(10) ,
+	ChangedSumAssured money,
+	ChangedTotalPremium money ,
+	ChangedTotalPayment money ,
+	ChangedNominee varchar(50),
+	ChangedRelation varchar(50),
+	ChangedNomineeAddress varchar(250) ,
+	ChangedNomineeTelephone varchar(10),
+	ChangedNomineeDOB date
+
 insert into Endorsement(EndorsementId,CustomerId,ProductId,Policyno,ChangesMade,Status,CreatedDate) values(1, 100, 1, 4, '{skjdh}', 'InProgress', '2019-09-09')
 update Endorsement set Status='InProgress' where EndorsementId=1
 select * from Endorsement
+
+create table AdminLogin
+(
+	AdminId int Identity not null,
+	UserName varchar(25) not null unique,
+	Password varchar(25) not null,
+	age int not null,
+)
+drop table AdminLogin
